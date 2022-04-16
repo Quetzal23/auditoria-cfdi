cc6eb70fae79430540c84546d9325892da205afb
Quetzal23 <201800048@etudiantes.upqroo.edu.mx>
2022-04-16 16:29:24 -0500

    Cambio en main

--- main.py
+++ main.py
@@ -3,22 +3,9 @@ from tkinter import *
 from view.acceso.login import Login
 from view.index import Index
 
-from view.registroEmpresa.parametro_empresa import Parametros_Empresas
-from view.registroEmpresa.catalogo_trabajo import Catalogo_Centro
-from view.menuArchivo.contadores_publicos import Contadores_Publicos
-from view.menuArchivo.usuraios import Usuarios
-
-'''
-# Views #
-Parametros_Empresas(window)
-Catalogo_Centro(window)
-'''
-
 if __name__ == '__main__':
     window = Tk()
-    #login = Login(window)
-    index = Index(window)
-
-    #s = Usuarios(window)
+    login = Login(window)
+    #index = Index(window)
 
     window.mainloop()
---
+++ view/catalogos/ocupaciones_oficios.py
@@ -0,0 +1,82 @@
+from tkinter import *
+from tkinter import ttk
+from tkinter.ttk import Treeview
+
+from view.upperentry import UpperEntry
+
+class Oficios:
+    def __init__(self, window):
+        wind_width  = 656
+        wind_height = 272
+        wind_geometry = str(wind_width) + 'x' + str(wind_height)
+        self.wind = window
+        self.wind.title('OCUPACIONES Y OFICIOS')
+        self.wind.geometry(wind_geometry)
+        self.wind.resizable(0,0)
+        self.wind.protocol('WM_DELETE_WINDOW', )
+        self.create_widgets(self.wind)
+
+    def on_closing(self):
+        self.wind.destroy()
+
+    def create_widgets(self, wind):
+        parametros = LabelFrame(wind, relief='flat')
+        parametros.grid(row=0, column=0, padx=10, pady=8, sticky=NSEW)
+
+        self.widget_frame_buttons(parametros)
+        self.widget_frame_treeview(parametros)
+        self.widget_frame_datos(parametros)
+
+    def widget_frame_buttons(self, parametros):
+        frame_top = LabelFrame(parametros, relief='flat')
+        frame_top.grid(row=0, column=0, columnspan=2, sticky=NSEW)
+        self.button_new = Button(frame_top, text='New')
+        self.button_new.grid(column=0, row=0, padx=5, pady=5, sticky=W)
+        self.button_save = Button(frame_top, text='Save')
+        self.button_save.grid(column=1, row=0, padx=5, pady=5, sticky=W)
+        self.button_edit = Button(frame_top, text='Edit')
+        self.button_edit.grid(column=2, row=0, padx=5, pady=5, sticky=W)
+        self.button_delete = Button(frame_top, text='Delete')
+        self.button_delete.grid(column=3, row=0, padx=5, pady=5, sticky=W)
+        self.button_print = Button(frame_top, text='Print')
+        self.button_print.grid(column=4, row=0, padx=5, pady=5, sticky=W)
+        
+        self.button_exit = Button(frame_top, text='Exit', command=self.on_closing)
+        self.button_exit.grid(column=6, row=0, padx=5, pady=5, sticky=EW)
+
+    def widget_frame_treeview(self, parametros):
+        frame_left = LabelFrame(parametros, relief='flat')
+        frame_left.grid(row=1, column=0, padx=0, pady=0, sticky=NSEW)
+
+        self.treeview = Treeview(frame_left, columns=[f"#{n}" for n in range(0, 2)], height=8) #columns=[f"#{n}" for n in range(0, 2)],
+        self.treeview.heading("#0", text='', anchor=CENTER)
+        self.treeview.heading("#1", text='Clave', anchor=CENTER)
+        self.treeview.heading("#2", text='Descripción', anchor=CENTER)
+
+        self.treeview.column('#0', stretch=False, width=0, minwidth=0)
+        self.treeview.column('#1', stretch=False, width=100, minwidth=64)    #220
+        self.treeview.column('#2', stretch=False, width=208, minwidth=208)
+        #self.treeview.config(show='headings')
+        self.treeview.grid(row=0, column=0, columnspan=2, sticky="nsew")
+
+        scrollbar = Scrollbar(frame_left, orient=VERTICAL, command=self.treeview.yview)
+        scrollbar.grid(row=0, column=3, sticky="nse")
+        self.treeview.configure(yscrollcommand=scrollbar.set)
+        sc = Scrollbar(frame_left, orient=HORIZONTAL, command=self.treeview.xview)
+        sc.grid(row=1, column=0, columnspan=2, sticky="ew")
+        self.treeview.configure(xscrollcommand=sc.set)
+
+    def widget_frame_datos(self, parametros):
+        frame_right = LabelFrame(parametros, relief='flat')
+        frame_right.grid(row=1, column=1, padx=0, pady=0, sticky=NSEW)
+
+        clave_label = Label(frame_right, text='Clave')
+        clave_label.grid(row=1, column=0, sticky=W, padx=2)
+        self.clave_entry = UpperEntry(frame_right)
+        self.clave_entry.grid(row=2, column=0, sticky=EW, padx=2)
+
+        descripcion_label = Label(frame_right, text='Descripción')
+        descripcion_label.grid(row=3, column=0, sticky=W, padx=2)
+        self.descripcion_entry = UpperEntry(frame_right, width=48)
+        self.descripcion_entry.grid(row=4, column=0, columnspan=2, sticky=EW, padx=2)
+        
